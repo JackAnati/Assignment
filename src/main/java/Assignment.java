131b6e2e5300698b2e792265b988ea9112148c71
JackAnati <Anatijack0@gmail.com>
2020-02-18 17:04:08 +0200

    Changes

--- src/main/java/Assignment.java
+++ src/main/java/Assignment.java
@@ -2,7 +2,7 @@ public class Assignment {
     public static void main(String args[]){
 
         System.out.println("Welcome to IT");
-
-
+        System.out.println("Make this a year of learning ");
+        System.out.println("MOTTO: Creating Futures");
     }
 }
